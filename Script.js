c57904b5e600a5fcb89aae815ac1b706bc4fa395
mavrodia1234 <114522241+mavrodia1234@users.noreply.github.com>
2023-06-12 15:16:00 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+var NavLinks = document.getElementById("NavLinks");
+
+function ShowMenu(){
+    NavLinks.style.right = ("0px");
+}
+
+function HideMenu(){
+    NavLinks.style.right = ("-200px");
+}
+
+
